76fba928cdbd00a669ab772710727cc7076205bf
suezan8905 <sussanobi1999@gmail.com>
2025-05-08 12:33:14 +0100

    update

--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -1,12 +1,22 @@
-import { NavLink, Outlet } from "react-router";
+import { NavLink, Outlet, useNavigate, useLocation } from "react-router";
 import Container from "../../components/Container";
 import MetaArgs from "../../components/MetaArgs";
 import { settingsLinks } from "../../libs/constants";
 import { useAuth } from "../../store";
 import TimeAgo from "timeago-react";
+import { useEffect } from "react";
 
 export default function Settings() {
   const { user } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const path = location.pathname === "/settings";
+
+  useEffect(() => {
+    if (path) {
+      navigate("/settings/update-password");
+    }
+  }, [navigate, path]);
 
   const formatTime = (time) => {
     return <TimeAgo datetime={time} locale="en-US" />;
